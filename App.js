2d090cf47340eda3792a2a69588c872d97e94a89
Max de Vries <max.devries.1995@gmail.com>
2021-04-21 21:14:31 +0200

    Add card component

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Header from './src/components/Header';
+import Card from './src/components/Card';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
+      <Card />
     </View>
   );
 }
