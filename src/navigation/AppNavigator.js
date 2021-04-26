b857d975d607292b54ff7024972c4bfb6e826e87
Max de Vries <max.devries.1995@gmail.com>
2021-04-26 21:03:41 +0200

    Add drawer navigation with hamburger menu

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { MaterialIcons } from '@expo/vector-icons';
 
 import NewsListScreen from '../screens/NewsListScreen';
 import NewsDetailsScreen from '../screens/NewsDetailsScreen';
 import FavoritesScreen from '../screens/FavoritesScreen';
+import AboutScreen from '../screens/AboutScreen';
 
 const Stack = createStackNavigator();
 const Tabs = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+
+const HeaderLeft = () => {
+  const navigation = useNavigation();
+
+  return (
+    <MaterialIcons name="menu" size={24} onPress={() => navigation.openDrawer()} />
+  );
+}
 
 function HomeNavigator() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerLeft: () => <HeaderLeft />
+      }}
+    >
       <Stack.Screen
         name="NewsList"
         component={NewsListScreen}
@@ -34,32 +49,57 @@ function HomeNavigator() {
 
 function FavoritesNavigator() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerLeft: () => <HeaderLeft />
+      }}
+    >
       <Stack.Screen name="Favorites" component={FavoritesScreen} />
     </Stack.Navigator>
+  );
+}
+
+function AboutNavigator() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerLeft: () => <HeaderLeft />
+      }}
+    >
+      <Stack.Screen name="About" component={AboutScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function TabsNavigator() {
+  return (
+    <Tabs.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: () => {
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = 'home';
+          } else if (route.name === 'Favorites') {
+            iconName = 'favorite';
+          }
+
+          return <MaterialIcons name={iconName} size={24} />
+        }
+      })}
+    >
+      <Tabs.Screen name="Home" component={HomeNavigator} />
+      <Tabs.Screen name="Favorites" component={FavoritesNavigator} />
+    </Tabs.Navigator>
   );
 }
 
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Tabs.Navigator
-        screenOptions={({route}) => ({
-          tabBarIcon: () => {
-            let iconName;
-            if (route.name === 'Home') {
-              iconName = 'home';
-            } else if (route.name === 'Favorites') {
-              iconName = 'favorite';
-            }
-
-            return <MaterialIcons name={iconName} size={24} />
-          }
-        })}
-      >
-        <Tabs.Screen name="Home" component={HomeNavigator} />
-        <Tabs.Screen name="Favorites" component={FavoritesNavigator} />
-      </Tabs.Navigator>
+      <Drawer.Navigator>
+        <Drawer.Screen name="News" component={TabsNavigator} />
+        <Drawer.Screen name="About" component={AboutNavigator} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
